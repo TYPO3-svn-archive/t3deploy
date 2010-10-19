04e6a256aa8aa48dd3394bf603b2920c3f1eac28
axeljung01 <axeljung01@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-10-19 15:21:15 +0000

    Upload to TER.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3deploy/trunk@39337 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "t3deploy".
 #
-# Auto generated 19-10-2010 10:24
+# Auto generated 19-10-2010 17:20
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.1',
+	'version' => '0.0.2',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.2.0-0.0.0',
@@ -37,7 +37,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:12:"dispatch.php";s:4:"b252";s:12:"ext_icon.gif";s:4:"f19a";s:17:"ext_localconf.php";s:4:"ee9f";s:14:"ext_tables.php";s:4:"845e";s:48:"classes/class.tx_t3deploy_databaseController.php";s:4:"dcd8";s:38:"classes/class.tx_t3deploy_dispatch.php";s:4:"2361";s:61:"tests/class.tx_t3deploy_tests_databaseController_testcase.php";s:4:"1b51";s:51:"tests/class.tx_t3deploy_tests_dispatch_testcase.php";s:4:"9bd1";s:43:"tests/fixtures/testextension/ext_tables.sql";s:4:"47eb";}',
+	'_md5_values_when_last_written' => 'a:10:{s:12:"dispatch.php";s:4:"b252";s:12:"ext_icon.gif";s:4:"f19a";s:17:"ext_localconf.php";s:4:"ee9f";s:14:"ext_tables.php";s:4:"845e";s:48:"classes/class.tx_t3deploy_databaseController.php";s:4:"dcd8";s:38:"classes/class.tx_t3deploy_dispatch.php";s:4:"2361";s:14:"doc/manual.sxw";s:4:"584f";s:61:"tests/class.tx_t3deploy_tests_databaseController_testcase.php";s:4:"1b51";s:51:"tests/class.tx_t3deploy_tests_dispatch_testcase.php";s:4:"9bd1";s:43:"tests/fixtures/testextension/ext_tables.sql";s:4:"47eb";}',
 	'suggests' => array(
 	),
 );
